795d93e9c88504f5ce8726c56f7c6d902813812c
James  Farrell <James@Jamess-MacBook-Pro.local>
2016-09-26 13:19:31 +0100

    Initial push

---
+++ src/main/java/com/james/spring/test/App.java
@@ -0,0 +1,19 @@
+package com.james.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/james/spring/test/beans/beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		person.speak();
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
